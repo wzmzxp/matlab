2f70f922c5fcec3986954137c4ddc50dbe782cf6
wzmzxp <2581806204@qq.com>
2018-12-27 15:23:32 +0800

    GD无注释

--- GD.m
+++ GD.m
@@ -1,20 +1,13 @@
 function z=GD(fun,d)
-    syms u1 u2 u3 u4 u5 u6                                 % 通过符号变量将匿名函数转换为符号函数
+    syms u1 u2 u3 u4 u5 u6                               
     str=[u1,u2,u3,u4,u5,u6];
     y = fun(str(1:d));
     syms z1 
     z=[z1]
     for i=1:d
-%         z(i)= matlabFunction(diff(y,str(i)))
         z(i)=diff(y,str(i))
     end
-%     if d==1
-%         y = fun([u1])
-%         z = matlabFunction(diff(y,u1))
-%     end
-%     if d==2
-%         y = fun([u1,u2])
-%         z(1) = matlabFunction(diff(y,u1))
+
 
     
     
